eb49ea4660e915b6806897d6485489d91f4e0a0d
Mario <Maikelsgc@gmail.com>
2013-03-06 11:28:40 +0100

    Trabajando con las clase Drawable

--- Asteroides/gen/com/example/asteroides/R.java
+++ Asteroides/gen/com/example/asteroides/R.java
@@ -25,8 +25,9 @@ public final class R {
         public static final int asteroide1=0x7f020000;
         public static final int asteroide2=0x7f020001;
         public static final int asteroide3=0x7f020002;
-        public static final int ic_launcher=0x7f020003;
-        public static final int space=0x7f020004;
+        public static final int degradado=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
+        public static final int space=0x7f020005;
     }
     public static final class id {
         public static final int LinearLayout1=0x7f0a0004;
--- Asteroides/src/com/example/asteroides/Puntuaciones.java
+++ Asteroides/src/com/example/asteroides/Puntuaciones.java
@@ -2,6 +2,9 @@ package com.example.asteroides;
 
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class Puntuaciones extends ListActivity {
 
@@ -12,6 +15,14 @@ public class Puntuaciones extends ListActivity {
 		setContentView(R.layout.puntuaciones);
 		setListAdapter(new Miadaptador(this, Asteroides.almacen.listaPuntuaciones(10)));
 				
+	}
+
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		// TODO Auto-generated method stub
+		super.onListItemClick(l, v, position, id);
+		Object o = getListAdapter().getItem(position);
+		Toast.makeText(this, "Selección: "+ Integer.toString(position)+" - "+o.toString(), Toast.LENGTH_LONG).show(); 
 	}
 
 }
